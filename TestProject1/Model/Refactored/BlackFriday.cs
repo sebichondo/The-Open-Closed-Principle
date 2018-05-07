953ba8ea05f94657ec4da29c51d25e7eaab4443f
sebichondo <sebichondo@gmail.com>
2018-05-07 16:43:03 +0300

    Added OCP project files

---
+++ TestProject1/Model/Refactored/BlackFriday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommerceProject.Model.Refactored
+{
+    public class BlackFriday : IPriceRule
+    {
+        public decimal CalculatePrice(OrderItem item)
+        {
+            decimal total = 0m;
+            total += item.Quantity * 1m;
+            
+            return total-1000;
+        }
+
+        public bool IsMatch(OrderItem item)
+        {
+            return item.Sku.StartsWith("BF");
+        }
+    }
+}
